8bab0c65366a4ac6096056a1a5ee60995aa9a0f3
Nuzaim <nuzaim.t123@gmail.com>
2023-12-22 10:25:10 +0530

    added feature for creating users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
+const bodyParser = require("body-parser");
+const Users = require("../models/users.js");
+
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.route('/')
+  /* GET users listing. */
+  .get(function(req, res, next) {
+    res.send('get operation is not supported on /users.');
+  })
+  // add a new user
+  .post(function(req, res, next) {
+    Users.create(req.body)
+      .then((user) => {
+        console.log("New user created: ", user);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(user);
+      })
+      .catch(err => next(err))
+  });
 
 module.exports = router;
